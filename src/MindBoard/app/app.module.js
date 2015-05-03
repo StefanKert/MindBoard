a604d00459d1ce31f6fd0a73e0496a6d0872d7d1
Stefan Kert <stefan.kert@inode.at>
2015-05-03 14:14:26 +0200

    Added first implementation for mindboard

---
+++ src/MindBoard/app/app.module.js
@@ -0,0 +1,12 @@
+﻿var app = angular.module("MindBoard", ["firebase"]);
+
+
+angular.module("MindBoard").controller("SampleCtrl", function ($scope, $firebaseArray) {
+    var ref = new Firebase("https://glowing-heat-6370.firebaseio.com/");
+    $scope.messages = $firebaseArray(ref);
+    $scope.addMessage = function () {
+        $scope.messages.$add({
+            text: $scope.newMessageText
+        });
+    };
+});
---
+++ src/MindBoard/gruntfile.js
@@ -0,0 +1,101 @@
+﻿module.exports = function (grunt) {
+    grunt.initConfig({
+        clean: {
+            options: {
+                'no-write': true
+            },
+            wwwroot: ["wwwroot/lib/**", "temp/", "wwwroot/app"]
+        },
+        bower: {
+            install: {
+                options: {
+                    targetDir: "vendor",
+                    layout: "byComponent",
+                    cleanTargetDir: false
+                }
+            }
+        },
+        copy: {
+            all: {
+                files: [{
+                    expand: true,
+                    src: ['app/**/*.html'],
+                    dest: 'wwwroot/'
+                }]
+            }
+        },
+        concat: {
+            all: {
+                src: ['vendor/angular/*.js', 'vendor/firebase/firebase.js', 'vendor/angular-firebase/ng-firebase.js', 'vendor/jquery/js/.js', 'vendor/**/*.js', '!vendor/angular-firebase/demo/*.*', 'app/app.module.js', 'app/**/*.js', '!vendor/**/*.min.js'],
+                dest: 'wwwroot/js/combined.js'
+            }
+        },
+        jshint: {
+            files: ['app/**/*.js'],
+            options: {
+                '-W069': false,
+            }
+        },
+        uglify: {
+            all: {
+                src: ['wwwroot/js/combined.js'],
+                dest: 'wwwroot/js/combined.min.js'
+            }
+        },
+        watch: {
+            js: {
+                files: ['**/*.js'],
+                tasks: ['concat', 'copy'],
+                options: {
+                    livereload: true,
+                }
+            },
+            html: {
+                files: ['app/**/*.html'],
+                tasks: ['copy'],
+                options: {
+                    livereload: true,
+                }
+            }
+        },
+        ngAnnotate: {
+            compile: {
+                files: [
+                    {
+                        src: 'wwwroot/js/combined.js',
+                        dest: 'wwwroot/js/combined.js'
+                    }
+                ]
+            }
+        },
+        htmlmin: {   
+            dist: { 
+                options: {   
+                    removeComments: true,
+                    collapseWhitespace: true
+                },
+                files: {       
+                    'wwwroot/index.html': 'index.html'
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('watch', ['watch:js', 'watch:html']);
+    grunt.registerTask("default", ["bower:install"]);
+    grunt.registerTask("all", ['clean', 'bower', 'concat', 'copy', 'jshint']);
+
+    grunt.registerTask("updateJs", ['clean', 'bower', 'jshint', 'concat', 'ngAnnotate', 'uglify'])
+    grunt.registerTask("updateHtms", ['htmlmin'])
+
+
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-bower-task');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-ng-annotate');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+};
